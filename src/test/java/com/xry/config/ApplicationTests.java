899ad337a602a6776ca7a720d7d0ec12c24cf196
xry-zero <2847779149@qq.com>
2020-05-04 09:23:20 +0800

    init

---
+++ src/test/java/com/xry/config/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xry.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
